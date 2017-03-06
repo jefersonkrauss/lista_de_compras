e37ef1289f68ca1b3bdae3a5caac6ae7c16d4649
jefersonkrauss <jeferson.krauss@gmail.com>
2017-03-05 23:36:50 -0300

    Iniciando conceitos

---
+++ app/src/main/java/br/com/mmbj/listadecompras/ItemListActivity.java
@@ -0,0 +1,28 @@
+package br.com.mmbj.listadecompras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+
+    }
+}
